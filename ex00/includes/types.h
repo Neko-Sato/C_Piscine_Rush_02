3b0dd9a99e1c09b6d8de3c5c22429f4dde84c6c3
hshimizu <hisatosi35350@gmail.com>
2023-03-24 22:20:23 +0900

    とりあえずmainを書いた

--- ex00/includes/types.h
+++ ex00/includes/types.h
@@ -1,16 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   types.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/24 20:21:55 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/24 20:22:21 by hshimizu         ###   ########.fr       */
+/*   Created: 2023/03/24 21:18:23 by hshimizu          #+#    #+#             */
+/*   Updated: 2023/03/24 21:43:49 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char *argv[]){
+#ifndef TYPES_H
+# define TYPES_H
 
-	return 0;
-}
+# define ULONG long long;
+# define STRING *char
+
+typedef struct s_numbers_dict	
+{
+	STRING	key;
+	ULONG	valus;
+}			t_numbers_dict;
+
+#endif
---
+++ ex00/run.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 20:21:55 by hshimizu          #+#    #+#             */
+/*   Updated: 2023/03/24 22:17:08 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush_02.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * 実行を書くファイルです
+ * 引数が1の場合デフォルトの辞書を生成します
+ * 引数が2の場合pathから辞書型を生成します
+ *
+ * また、引数の文字列から数値に変換します
+ *
+ * どちらも不正な値ならばNULLを返しプログラムを終了させます
+ * NULL以外が返ってきたら関数に値を渡します
+ */
+
+int	main(int argc, STRING argv[])
+{
+	t_numbers_dict	*dict;
+	ULONG			*num;
+
+	dict = NULL;
+	num = NULL;
+	if (argc == 2)
+	{
+		dict = get_default_dict();
+		num = ft_atoll(argv[1]);
+	}
+	else if (argc == 3)
+	{
+		dict = create_dict(argv[1]));
+		num = ft_atoll(argv[2]);
+	}
+	if (!num)
+		write(STDOUT_FILENO, "Error\n", 6);
+	else if (!dict)
+		write(STDOUT_FILENO, "Dict Error\n", 11);
+	else
+	{
+		rush_02(*dict, *num);
+		free(dict);
+		free(num);
+	}
+}
