5d59f6562e27fe77b4a639150470870013eb98a3
toshota <toshota@student.42tokyo.jp>
2023-03-25 15:45:37 +0900

    ft_atoll.hのファイルを追加

--- ex00/includes/ft_atoll.h
+++ ex00/includes/ft_atoll.h
@@ -3,10 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoll.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 22:18:27 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/24 22:24:57 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/25 14:22:42 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_ATOLL_H
+# define FT_ATOLL_H
+# include <stdlib.h>
+long long	*ft_atoll(const char *str);
+
+#endif
